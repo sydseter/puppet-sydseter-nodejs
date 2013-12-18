60b459fd412c99e2f78eb0a8a46edf059fade6e9
Johan Sydseter <johan@aptoma.com>
2013-12-18 17:55:11 +0100

    make it possible to install node without using sudo

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,10 @@
 #  }
 #   
 #
-class nodejs ($build = 'ubuntu-12-04_linux-headers-3.5.0-36-generic-v0.10.17') {
+class nodejs (
+  $build = 'ubuntu-12-04_linux-headers-3.5.0-36-generic-v0.10.17',
+  $user  = 'vagrant'
+){
 
   package {'g++':
     ensure => installed
@@ -38,22 +41,35 @@ class nodejs ($build = 'ubuntu-12-04_linux-headers-3.5.0-36-generic-v0.10.17') {
     ensure => installed
   }
 
-
-
-  file { '/var/tmp/build/node':
-    ensure => present,
-    require => Package['apache2-utils', 'libssl-dev', 'curl', 'g++', 'python-software-properties'],
-    mode   => 0711,
-    source => ["puppet:///modules/nodejs/${build}"]
+  exec {"echo 'export PATH=\$HOME/local/bin:\$PATH' >> /home/${user}/.bashrc":
+    path        => '/usr/local/bin:/usr/bin:/usr/sbin:/bin',
+    subscribe   => File["/home/${user}/local"],
+    refreshonly => true
   }
 
+  file { ['/var/tmp/build',"/home/${user}/local"]:
+    ensure => 'directory',
+    owner  => "${user}",
+    mode   => 0755
+  }
+
+  file { '/var/tmp/build/node':
+    ensure  => 'present',
+    require => Package['apache2-utils', 'libssl-dev', 'curl', 'g++', 'python-software-properties'],
+    mode    => 0711,
+    owner   => "${user}",
+    recurse => true,
+    source  => ["puppet:///modules/nodejs/${build}"]
+  }
 
   exec { 'make install':
-    cwd     => '/var/tmp/build/node',
-    path => '/usr/local/bin:/usr/bin:/usr/sbin:/bin',
-    subscribe => File['/var/tmp/build/node'],
+    cwd         => '/var/tmp/build/node',
+    user        => "${user}",
+    path        => '/usr/local/bin:/usr/bin:/usr/sbin:/bin',
+    require     => File['/var/tmp/build/node',"/home/${user}/local"],
+    subscribe   => File['/var/tmp/build/node',"/home/${user}/local"],
     refreshonly => true,
-    timeout => 0
+    timeout     => 0
   }
 
 }
